d82ab132056b7d3c5e531d8e879e3c2ff280e3af
Lukas <oss.heindl+git@protonmail.com>
2024-05-14 23:18:47 +0200

    add.bash not working properly -> comment out problematic part

--- add.bash
+++ add.bash
@@ -8,8 +8,9 @@ fi
 cp "./sections/example.tex" "./sections/${1}.tex"
 sed -i 's/\\section{Example section}/\\section{'"${2}"'}/' "./sections/${1}.tex"
 
-line="$(sed -E '/^([[:space:]]*%.*)?$/d' main.tex | sed -nE -e '/^[[:space:]]*\\end\{condMulticols\}[[:space:]]*(%.*)?$/{x;p;d}' -e 'x' | sed -e 's/[\/&]/\\&/g')"
-sed -i '/'"${line}"'/a \\t\\input\{sections\/'"${1}"'\}' main.tex
+# does not work correctly in general
+# line="$(sed -E '/^([[:space:]]*%.*)?$/d' main.tex | sed -nE -e '/^[[:space:]]*\\end\{condMulticols\}[[:space:]]*(%.*)?$/{x;p;d}' -e 'x' | sed -e 's/[\/&]/\\&/g')"
+# sed -i '/'"${line}"'/a \\t\\input\{sections\/'"${1}"'\}' main.tex
 
 # set this environment variable to enable this feature
 if [[ -n "$INTERACTIVE" ]] ; then
